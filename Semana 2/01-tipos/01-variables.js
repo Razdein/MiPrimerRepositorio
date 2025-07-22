305623cca7d535b92294f0fee2e7d6b8ae0499e7
Andrés Marquez <andrew98mp@gmail.com>
2025-07-22 07:42:08 -0300

    empezando javascript

---
+++ Semana 2/01-tipos/01-variables.js
@@ -0,0 +1,9 @@
+let nombre;
+let NombreCompleto ="Hola Mundo";
+let nombreCompleto;
+let nombre_completo;
+console.log(nombreCompleto);
+
+let apellido;
+let animal;
+apellido = "Marquez";
---
+++ Semana 2/java.js
@@ -0,0 +1,7 @@
+let num1 = 5;
+let num2 = 8;
+let num3 = 2;
+
+let promedio = (num1 + num2 + num3) / 3;
+
+console.log("El promedio es:", promedio);
